2fecf26053ee3717f397afcfb1660acd68cb335f
alimy <alimy@gility.net>
2020-03-07 14:59:33 +0800

    support chi/mux/httprouter template project generate

--- internal/generator/templates_mir.go
+++ internal/generator/templates_mir.go
@@ -15,4 +15,34 @@ func init() {
 		"mirc/routes/v1/site.go": {"mir/gin_mirc_routes_site_v1.tmpl", false, false},
 		"mirc/routes/v2/site.go": {"mir/gin_mirc_routes_site_v2.tmpl", false, false},
 	}
+	tmplFiles["chi"] = tmplInfos{
+		"Makefile":               {"mir/makefile.tmpl", false, false},
+		"README.md":              {"mir/readme.tmpl", false, false},
+		"go.mod":                 {"mir/chi_go_mod.tmpl", true, false},
+		"main.go":                {"mir/chi_main.tmpl", false, false},
+		"mirc/main.go":           {"mir/chi_mirc_main.tmpl", true, false},
+		"mirc/routes/site.go":    {"mir/chi_mirc_routes_site.tmpl", false, false},
+		"mirc/routes/v1/site.go": {"mir/chi_mirc_routes_site_v1.tmpl", false, false},
+		"mirc/routes/v2/site.go": {"mir/chi_mirc_routes_site_v2.tmpl", false, false},
+	}
+	tmplFiles["mux"] = tmplInfos{
+		"Makefile":               {"mir/makefile.tmpl", false, false},
+		"README.md":              {"mir/readme.tmpl", false, false},
+		"go.mod":                 {"mir/mux_go_mod.tmpl", true, false},
+		"main.go":                {"mir/mux_main.tmpl", false, false},
+		"mirc/main.go":           {"mir/mux_mirc_main.tmpl", true, false},
+		"mirc/routes/site.go":    {"mir/mux_mirc_routes_site.tmpl", false, false},
+		"mirc/routes/v1/site.go": {"mir/mux_mirc_routes_site_v1.tmpl", false, false},
+		"mirc/routes/v2/site.go": {"mir/mux_mirc_routes_site_v2.tmpl", false, false},
+	}
+	tmplFiles["httprouter"] = tmplInfos{
+		"Makefile":               {"mir/makefile.tmpl", false, false},
+		"README.md":              {"mir/readme.tmpl", false, false},
+		"go.mod":                 {"mir/httprouter_go_mod.tmpl", true, false},
+		"main.go":                {"mir/httprouter_main.tmpl", false, false},
+		"mirc/main.go":           {"mir/httprouter_mirc_main.tmpl", true, false},
+		"mirc/routes/site.go":    {"mir/httprouter_mirc_routes_site.tmpl", false, false},
+		"mirc/routes/v1/site.go": {"mir/httprouter_mirc_routes_site_v1.tmpl", false, false},
+		"mirc/routes/v2/site.go": {"mir/httprouter_mirc_routes_site_v2.tmpl", false, false},
+	}
 }
